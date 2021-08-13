0e57f65049de8229c6dbe312925e877b44f43064
UmamaheshMaxwell <maheshmeka16@gmail.com>
2021-08-13 13:33:33 +0530

    javascript basics

---
+++ 6.functions/1.regular-function.js
@@ -0,0 +1,8 @@
+function getName(){
+    //console.log('Hello')
+    return "Scott Desatnick"
+    // return
+}
+
+var userName = getName()
+console.log(userName)
---
+++ app.js
@@ -0,0 +1,12 @@
+console.log(this)
+console.log(window)
+var name ="uma" // Global variable
+function sample(){
+   var  name ="mahesh" // Local Variable
+}
+
+
+//console.log('Hey')
+// console.warn('Please check this')
+// console.info('Provide more info please')
+// console.error('There is an error')
