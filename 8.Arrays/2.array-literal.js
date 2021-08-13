80d0d11f0a70be3a8cf51bf56b302067862eb45f
UmamaheshMaxwell <maheshmeka16@gmail.com>
2021-08-13 18:02:48 +0530

    added functions, objects, arrays

---
+++ 8.Arrays/2.array-literal.js
@@ -0,0 +1,7 @@
+var data = ["Earth", "Mars", "Venus", "Titan"];
+
+console.log(data)
+console.log(data[0])
+console.log(data[1])
+console.log(data[2])
+console.log(data[3])
--- app.js
+++ app.js
@@ -10,3 +10,12 @@ function sample(){
 // console.warn('Please check this')
 // console.info('Provide more info please')
 // console.error('There is an error')
+
+var a = 1 + 2 + "uma" + 3 + 4
+console.log(a, typeof a)
+
+var value = undefined + " Hello"
+console.log(value, typeof value)
+
+var output = null + " Hey"
+console.log(output, typeof output)
